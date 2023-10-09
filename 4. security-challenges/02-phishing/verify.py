e17a872ba6dfb4d6438ca58dff134eaa02b47d3d
Bram Van Impe <u0152691@ucll.be>
2023-10-09 10:04:59 +0200

    Fix error in verify script of 02-phishing

--- 4. security-challenges/02-phishing/verify.py
+++ 4. security-challenges/02-phishing/verify.py
@@ -14,7 +14,7 @@ def verify():
     
     assert lines[0] == 'a264c67fdbed62ce10be6319b94f26054d54baf1', 'FAIL: line 1 Originating IP is incorrect'
     assert lines[1] == '10b78a2df83719e57a79dc354a05208212ae8ed3', 'FAIL: line 2 Attacker email is incorrect'
-    assert lines[0] == 'dbaea2cb42eb8f0e74583c7d4c87b722644ad9ac', 'FAIL: line 3 Victim email is incorrect'
+    assert lines[2] == 'dbaea2cb42eb8f0e74583c7d4c87b722644ad9ac', 'FAIL: line 3 Victim email is incorrect'
 
     print("SUCCESS: challenge completed.")
     return True
